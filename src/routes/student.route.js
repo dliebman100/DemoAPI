ff2c464d6fc14e644af6b5f8ea59200ee05781b7
Daniel Liebman <dliebman100@gmail.com>
2021-07-06 16:45:46 -0400

    added controller & refactored app

--- src/routes/student.route.js
+++ src/routes/student.route.js
@@ -1,7 +1,8 @@
 // handles all requests to /api/students endpoint
 module.exports = (app) => {
   const router = require("express").Router();
-
+  // configure routes bas on combo of URL/VERB
+  // GET all students
   router.get("/", function (req, res) {
     res.send("You have reached the router for api/student/");
   });
