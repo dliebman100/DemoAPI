e24e965a344385ed9465ba697227d2922b5e5abd
Daniel Liebman <dliebman100@gmail.com>
2021-07-06 16:48:03 -0400

    refactored GET handler for "/" to use new controller findAll()

--- src/routes/student.route.js
+++ src/routes/student.route.js
@@ -1,11 +1,15 @@
 // handles all requests to /api/students endpoint
 module.exports = (app) => {
+  // const router = require("express").Router();
+  const students = require("../controllers/student.controller");
+
   const router = require("express").Router();
-  // configure routes bas on combo of URL/VERB
+  // configure routes based on combo of URL/VERB
   // GET all students
-  router.get("/", function (req, res) {
-    res.send("You have reached the router for api/student/");
-  });
+  // router.get("/", function (req, res) {
+  //   res.send("You have reached the router for api/student/");
+  // });
+  router.get("/", students.findAll);
 
   app.use("/api/student", router);
 };
